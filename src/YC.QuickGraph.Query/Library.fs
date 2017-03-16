21092c304ec401b9ef6eadcac3fd6335ab803ab9
Sergey Svitkov <Sergey Svitkov>
2017-03-16 16:01:54 +0300

    Implemented IParserInput

--- src/YC.QuickGraph.Query/Library.fs
+++ src/YC.QuickGraph.Query/Library.fs
@@ -3,10 +3,10 @@ namespace YC.QuickGraph.Query
 open QuickGraph
 open Yard.Generators.GLL
 open Yard.Generators
-open YC.GLL.SPPF
 open YC.FST.AbstractLexing
 open YaccConstructor.API
 open Yard.Generators.GLL.ParserCommon
+open YC.QuickGraph.Query.GraphsRealisations
 
 module Library =
     
@@ -14,11 +14,16 @@ module Library =
         generate grammarFilePath "YardFrontend" "GllGenerator" None ["ExpandMeta"] [] :?> ParserSourceGLL
 
     let getInputGraph pathToFile (edgeObjectToString : 'EdgeObject -> string) =
-        new ParserInputGraph<'EdgeObject>(42, edgeObjectToString)
+        new ParserInputGraph<'EdgeObject>(3, edgeObjectToString)
 
-    (*let doSmth grammarFilePath = 
+    let parso parserSource input = 
+        AbstractParser.parse parserSource input true 
+
+    let doSmth grammarFilePath graphPath (edgeObjectToString : 'EdgeObject -> string)= 
         let parser = getParserSource grammarFilePath
-        let input =  *)
+        let input =  getInputGraph graphPath edgeObjectToString
+        parso parser input
+
     //let SPPFToSubgraph (sppf: SPPF) =
 
     //let SPPFToCFRelation (sppf: SPPF) = 
--- src/YC.QuickGraph.Query/ParserInputGraph.fs
+++ src/YC.QuickGraph.Query/ParserInputGraph.fs
@@ -4,79 +4,91 @@ open AbstractAnalysis.Common
 open QuickGraph
 open System.Runtime.CompilerServices
 
-type LexerEdge<'l ,'br  when 'l: equality> (s,e,t) =
-    inherit TaggedEdge<int,Option<'l*'br>>(s,e,t)
-    let l,br =
-        match t with
-        | Some (l,br) -> Some l, Some br
-        | None -> None, None
+module GraphsRealisations =
 
-    member this.BackRef = br
-    member this.Label = l
+    type LexerEdge<'l ,'br  when 'l: equality> (s,e,t) =
+        inherit TaggedEdge<int,Option<'l*'br>>(s,e,t)
+        let l,br =
+            match t with
+            | Some (l,br) -> Some l, Some br
+            | None -> None, None
 
-type ParserEdge<'EdgeObject>(s, e, t)=
-    inherit TaggedEdge<int, 'EdgeObject>(s, e, t)
+        member this.BackRef = br
+        member this.Label = l
 
-type ParserInputGraph<'EdgeObject>(initialVertices : int[], finalVertices : int[], ?edgeObjToString: ('EdgeObject -> string)) = 
-    inherit AdjacencyGraph<int, ParserEdge<'EdgeObject>>()
+    type ParserEdge<'EdgeObject>(s, e, t)=
+        inherit TaggedEdge<int, 'EdgeObject>(s, e, t)
 
-    member val InitStates = initialVertices 
-    member val FinalStates = finalVertices with get, set
-    member val ObjToString = match edgeObjToString with
-                             | Some func -> func
-                             | None -> fun value -> value.ToString()
+    type ParserInputGraph<'EdgeObject>(initialVertices : int[], finalVertices : int[], ?edgeObjToString: ('EdgeObject -> string)) = 
+        inherit AdjacencyGraph<int, ParserEdge<'EdgeObject>>()
 
-    member inline this.StringToToken str = str |> int
+        let StringToToken str = str |> int
 
-    member this.PrintToDot name (*(tokenToString : 'token -> string) (numToToken : int -> 'token)*) = 
-        use out = new System.IO.StreamWriter (name : string)
-        out.WriteLine("digraph AST {")
-        out.WriteLine "rankdir=LR"
-        for i = 0 to this.VertexCount - 1 do
-            out.Write (i.ToString() + "; ")
-        out.WriteLine()
-        for i in this.Vertices do
-            let edges = this.OutEdges i
-            for e in edges do
-                let tokenName = e.Tag |> this.ObjToString(*numToToken |> tokenToString*)
-                out.WriteLine (e.Source.ToString() + " -> " + e.Target.ToString() + "[label=\"" + tokenName + "\"]")
-        out.WriteLine("}")
-        out.Close()
+        member val InitStates = initialVertices 
+        member val FinalStates = finalVertices with get, set
+        member val ObjToString = match edgeObjToString with
+                                 | Some func -> func
+                                 | None -> fun value -> value.ToString()
 
-    new (initial : int, final : int, ?edgeObjToString: ('EdgeObject -> string)) = 
-        let objToStr = match edgeObjToString with
-                       | Some func -> func
-                       | None -> fun value -> value.ToString()
-        ParserInputGraph<_>([|initial|], [|final|], objToStr)
+        member this.PrintToDot name (*(tokenToString : 'token -> string) (numToToken : int -> 'token)*) = 
+            use out = new System.IO.StreamWriter (name : string)
+            out.WriteLine("digraph AST {")
+            out.WriteLine "rankdir=LR"
+            for i = 0 to this.VertexCount - 1 do
+                out.Write (i.ToString() + "; ")
+            out.WriteLine()
+            for i in this.Vertices do
+                let edges = this.OutEdges i
+                for e in edges do
+                    let tokenName = e.Tag |> this.ObjToString(*numToToken |> tokenToString*)
+                    out.WriteLine (e.Source.ToString() + " -> " + e.Target.ToString() + "[label=\"" + tokenName + "\"]")
+            out.WriteLine("}")
+            out.Close()
 
-    new (n : int, ?edgeObjToString: ('EdgeObject -> string)) =
-        let allVerticles = [|for i in 0 .. n - 1 -> i|]
-        let objToStr = match edgeObjToString with
-                       | Some func -> func
-                       | None -> fun value -> value.ToString()
-        ParserInputGraph<_>(allVerticles, allVerticles, objToStr)
+        new (initial : int, final : int, ?edgeObjToString: ('EdgeObject -> string)) = 
+            let objToStr = match edgeObjToString with
+                           | Some func -> func
+                           | None -> fun value -> value.ToString()
+            ParserInputGraph<_>([|initial|], [|final|], objToStr)
 
-type LinearInput (initialPositions, input:array<int<token>>) =
-    interface IParserInput with
-        member this.InitialPositions = initialPositions
-        [<MethodImpl(MethodImplOptions.AggressiveInlining)>]
-        member this.ForAllOutgoingEdges curPosInInput pFun =
-            if int curPosInInput < input.Length
-            then pFun input.[int curPosInInput] (curPosInInput + 1<positionInInput>)
+        new (n : int, ?edgeObjToString: ('EdgeObject -> string)) =
+            let allVerticles = [|for i in 0 .. n - 1 -> i|]
+            let objToStr = match edgeObjToString with
+                           | Some func -> func
+                           | None -> fun value -> value.ToString()
+            ParserInputGraph<_>(allVerticles, allVerticles, objToStr)
 
-    member this.Input = input
+        interface IParserInput with
+            member this.InitialPositions = 
+                Array.map (fun x -> x * 1<positionInInput>) this.InitStates
+            member this.ForAllOutgoingEdges curPosInInput pFun =
+                let outEdges = int curPosInInput |> this.OutEdges
+                outEdges |> Seq.iter 
+                    (fun e ->
+                        pFun ((e.Tag |> this.ObjToString |> StringToToken) * 1<token>) (e.Target * 1<positionInInput>)
+                    )
 
-    new (input:array<int<token>>) = LinearInput ([|0<positionInInput>|], input)
+    type LinearInput (initialPositions, input:array<int<token>>) =
+        interface IParserInput with
+            member this.InitialPositions = initialPositions
+            [<MethodImpl(MethodImplOptions.AggressiveInlining)>]
+            member this.ForAllOutgoingEdges curPosInInput pFun =
+                if int curPosInInput < input.Length
+                then pFun input.[int curPosInInput] (curPosInInput + 1<positionInInput>)
 
-type SimpleGraphInput<'tagType> (initialPositions, getTokenFromTag:'tagType -> int<token>) =
-    inherit AdjacencyGraph<int, TaggedEdge<int, 'tagType>>()
-    interface IParserInput with
-        member this.InitialPositions = initialPositions
-        [<MethodImpl(MethodImplOptions.AggressiveInlining)>]
-        member this.ForAllOutgoingEdges curPosInInput pFun =
-            let outEdges = int curPosInInput |> this.OutEdges
-            outEdges
-            |> Seq.iter 
-                (fun e ->
-                    pFun (getTokenFromTag e.Tag) (e.Target * 1<positionInInput>)
-                )
+        member this.Input = input
+
+        new (input:array<int<token>>) = LinearInput ([|0<positionInInput>|], input)
+
+    type SimpleGraphInput<'tagType> (initialPositions, getTokenFromTag:'tagType -> int<token>) =
+        inherit AdjacencyGraph<int, TaggedEdge<int, 'tagType>>()
+        interface IParserInput with
+            member this.InitialPositions = initialPositions
+            [<MethodImpl(MethodImplOptions.AggressiveInlining)>]
+            member this.ForAllOutgoingEdges curPosInInput pFun =
+                let outEdges = int curPosInInput |> this.OutEdges
+                outEdges
+                |> Seq.iter 
+                    (fun e ->
+                        pFun (getTokenFromTag e.Tag) (e.Target * 1<positionInInput>)
+                    )
